f1e50274def6a6fa0063b7944a92f9e482280b94
zzqt803 <xiaomazaiily@hotmail.com>
2025-12-10 23:24:46 +0800

    Init Commit:初始化

---
+++ netserver/01/tcpepoll.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main(){
+    std::cout<<"Hello world"<<std::endl;
+    std::cout<<"Hello world again"<<std::endl;
+    for(int i=0;i<5;i++){
+        std::cout<<"Iteration "<<i<<std::endl;
+    }
+    return 0;
+}
