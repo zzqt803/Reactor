162ad6c21d1db6ffd6b1c425e1d49cdde48cec0b
zzqt803 <xiaomazaiily@hotmail.com>
2025-12-10 23:36:39 +0800

    this is a test

--- netserver/01/tcpepoll.cpp
+++ netserver/01/tcpepoll.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 int main(){
     std::cout<<"Hello world"<<std::endl;
-    std::cout<<"Hello world again"<<std::endl;
+    std::cout<<"init github"<<std::endl;
     for(int i=0;i<5;i++){
         std::cout<<"Iteration "<<i<<std::endl;
     }
