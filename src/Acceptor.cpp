5b038a74dd9a3bc49f6451d569946986b22d8b67
zzqt803 <xiaomazaiily@hotmail.com>
2026-07-29 23:52:11 +0800

    构造函数

--- src/Acceptor.cpp
+++ src/Acceptor.cpp
@@ -1,4 +1,8 @@
 #include "Acceptor.h"
 #include "EventLoop.h"
+#include "InetAddr.h"
 
-
+Acceptor::Acceptor(EventLoop *loop, const InetAddr &listenAddr, bool reuseport)
+    : loop_(loop),
+      acceptSocket_(Socket::createNoblockingOrDie(listenAddr.family())),
+      acceptChannel_(loop, acceptSocket_.fd()), listening_(false) {}
