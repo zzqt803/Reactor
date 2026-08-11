91810d4922d8e48816611b0d82b5667751463a41
zzqt803 <xiaomazaiily@hotmail.com>
2026-08-12 01:18:59 +0800

    完善了该类

--- include/EventLoop.h
+++ include/EventLoop.h
@@ -1,12 +1,18 @@
 #pragma once
 
+#include "util/CurrentThread.h"
+
+#include <functional>
 #include <memory>
 #include <vector>
+#include <atomic>
+#include <mutex>
 
 class Channel;
 class EpollPoller;
 class EventLoop {
 public:
+  using Functor = std::function<void()>;
   EventLoop();
   ~EventLoop();
 
@@ -15,13 +21,45 @@ public:
 
   void loop();
 
+  void quit();
+
+  void runInLoop(Functor cb);
+  void queueInLoop(Functor cb);
+
+  void wakeup();
   void updateChannel(Channel *);
   void removeChannel(Channel *);
 
-private:
-  using ChannelList = std::vector<Channel *>;
-  std::unique_ptr<EpollPoller> poller_;
+  void assertInLoopThread() {
+    if (!isInLoopThread()) {
+      abortNotInLoopThread();
+    }
+  }
 
-  ChannelList activeChannels;
+  bool isInLoopThread() const { return threadId_ == CurrentThread::tid(); };
+  bool eventHandling() const { return eventHandling_; }
+  static EventLoop *getEventLoopOfCurrentThread();
+
+private:
+  void abortNotInLoopThread();
+  void handleRead(); // waked up
+  void doPendingFunctors();
+
+  using ChannelList = std::vector<Channel *>;
+
+  bool looping_;
+  std::atomic<bool> quit_;
+  bool eventHandling_;
+  bool callingPendingFunctors_;
+  int64_t iteration_;
+  const pid_t threadId_;
+  std::unique_ptr<EpollPoller> poller_;
+  int wakeupFd_;
+  std::unique_ptr<Channel> wakeupChannel_;
+
+  ChannelList activeChannels_;
   Channel *currentActiveChannel_;
+
+  std::mutex mutex_;
+  std::vector<Functor> pendingFunctors_;
 };
--- src/EventLoop.cpp
+++ src/EventLoop.cpp
@@ -1,27 +1,158 @@
 #include "EventLoop.h"
 #include "Channel.h"
 #include "EpollPoller.h"
+#include "Socket.h"
 
-EventLoop::EventLoop() { poller_ = std::make_unique<EpollPoller>(this); }
+#include "sys/eventfd.h"
+#include "util/Logger.h"
+#include <mutex>
+#include <sys/types.h>
 
-EventLoop::~EventLoop() {}
+//创建一个事件描述符，用来跨线程唤醒线程
+namespace {
+thread_local EventLoop *t_loopInThisThread = nullptr;
+
+const int kPollTimeMs = 10000;
+
+int createEventfd() {
+  int evtfd = ::eventfd(0, EFD_NONBLOCK | EFD_CLOEXEC); //非阻塞，执行时关闭
+  if (evtfd < 0) {
+    LOG_SYSFATAL("Failed in eventfd");
+  }
+  return evtfd;
+}
+} // namespace
+
+EventLoop *EventLoop::getEventLoopOfCurrentThread() {
+  return t_loopInThisThread;
+}
+
+EventLoop::EventLoop()
+    : looping_(false), quit_(false), eventHandling_(false),
+      callingPendingFunctors_(false), iteration_(0),
+      threadId_(CurrentThread::tid()),
+      poller_(std::make_unique<EpollPoller>(this)), wakeupFd_(createEventfd()),
+      currentActiveChannel_(nullptr) {
+  LOG_DEBUG("EventLoop created {0} in thread {1}", this, threadId_);
+  if (t_loopInThisThread) {
+    LOG_FATAL("Another EventLoop {0} exsits in this thread {1}",
+              t_loopInThisThread, threadId_);
+  } else {
+    t_loopInThisThread = this;
+  }
+  wakeupChannel_->setReadCallback(std::bind(&EventLoop::handleRead, this));
+  wakeupChannel_->enableReading();
+}
+
+EventLoop::~EventLoop() {
+  LOG_DEBUG("EventLoop {0} of thread {1} destructs in thread {2}", this,
+            threadId_, CurrentThread::tid());
+  wakeupChannel_->disableAll();
+  wakeupChannel_->remove();
+  ::close(wakeupFd_);
+  t_loopInThisThread = nullptr;
+}
 
 void EventLoop::loop() {
-  while (1) {
-    activeChannels.clear();
-    poller_->poll(10000, &activeChannels);
+  assert(!looping_);
+  assertInLoopThread();
+  looping_ = true;
+  while (!quit_) {
+    activeChannels_.clear();
+    poller_->poll(kPollTimeMs, &activeChannels_);
+    ++iteration_;
 
-    for (auto channel : activeChannels) {
+    eventHandling_ = true;
+    for (auto channel : activeChannels_) {
       currentActiveChannel_ = channel;
       currentActiveChannel_->handleEvent();
     }
+    currentActiveChannel_ = nullptr;
+    eventHandling_ = false;
+
+    doPendingFunctors();
+  }
+
+  LOG_TRACE("EventLoop {} stop looping", this);
+  looping_ = false;
+}
+
+void EventLoop::quit() {
+  quit_.store(true);
+  if (!isInLoopThread()) {
+    wakeup();
+  }
+}
+
+void EventLoop::runInLoop(Functor cb) {
+  if (isInLoopThread()) {
+    cb();
+  } else {
+    queueInLoop(std::move(cb));
+  }
+}
+
+void EventLoop::queueInLoop(Functor cb) {
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+    pendingFunctors_.push_back(std::move(cb));
+  }
+
+  if (!isInLoopThread() || callingPendingFunctors_) {
+    wakeup();
   }
 }
 
 void EventLoop::updateChannel(Channel *channel) {
+  assert(channel->ownerLoop() == this);
+  assertInLoopThread();
   poller_->updateChannel(channel);
 }
 
 void EventLoop::removeChannel(Channel *channel) {
+  assert(channel->ownerLoop() == this);
+  assertInLoopThread();
+  if (eventHandling_) {
+    assert(currentActiveChannel_ == channel ||
+           std::find(activeChannels_.begin(), activeChannels_.end(), channel) ==
+               activeChannels_.end());
+  }
   poller_->removeChannel(channel);
+}
+
+void EventLoop::abortNotInLoopThread() {
+  LOG_FATAL("EventLoop::abortNotInLoopThread - EventLoop {} was created in "
+            "thead {}, curent thread id= {}",
+            this, threadId_, CurrentThread::tid());
+}
+
+void EventLoop::wakeup() {
+  uint64_t one = 1;
+  ssize_t n = Socket::write(wakeupFd_, &one, sizeof one);
+  if (n != sizeof one) {
+    LOG_ERROR("EventLoop::wakeup() writes {} bytes instead of 8", n);
+  }
+}
+
+void EventLoop::handleRead() {
+  uint64_t one = 1;
+  ssize_t n = Socket::read(wakeupFd_, &one, sizeof one);
+  if (n != sizeof one) {
+    LOG_ERROR("EventLoop::handleRead() reads {} bytes instead of 8", n);
+  }
+}
+
+void EventLoop::doPendingFunctors() {
+  std::vector<Functor> functors;
+  callingPendingFunctors_ = true;
+
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+    functors.swap(pendingFunctors_);
+  }
+
+  for (const Functor &functor : functors) {
+    functor();
+  }
+  callingPendingFunctors_ = false;
 }
