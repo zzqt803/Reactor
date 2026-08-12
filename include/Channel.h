ae875738d388168de9586b6c624e51636bfd6f7d
zzqt803 <xiaomazaiily@hotmail.com>
2026-08-12 22:22:13 +0800

    完善了Channel

--- include/Channel.h
+++ include/Channel.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <functional>
+#include <memory>
+#include <string>
 
 class EventLoop;
 
@@ -46,14 +48,33 @@ public:
     events_ = kNoneEvent;
     update();
   }
+  bool isWriting() const { return events_ & kWriteEvent; }
+  bool isReading() const { return events_ & kReadEvent; }
+
+  //防止回调时回调函数所属类被析构
+  void tie(const std::shared_ptr<void>&);
 
   int fd() const { return fd_; }
   int events() const { return events_; }
   void setRevent(int revt) { revents_ = revt; }
+  bool isNoneEvent() const { return events_ == kNoneEvent; }
+
+  // for poller
+  int index() { return index_; }
+  void set_index(int idx) { index_ = idx; }
+
+  std::string reventsToString() const;
+  std::string eventsToString() const;
+
+  EventLoop *ownerLoop() const { return loop_; }
   void remove();
+
 private:
+  static std::string eventsToString(int fd,int ev);
   //调用loop的函数更新epoll状态
   void update();
+
+  void handleEventWithGuard();
 
   //事件
   static const int kNoneEvent;
@@ -64,13 +85,21 @@ private:
   EventLoop *loop_;
 
   //原生文件描述符
-  int fd_;
+  const int fd_;
 
   //关注的事件
   int events_;
 
   //发生的事件
   int revents_;
+
+  //用于标记在epoll中的状态
+  int index_;
+
+  std::weak_ptr<void> tie_;
+  bool tied_;
+  bool eventHandling_;
+  bool addedToLoop_;
 
   //对应事件发生时的回调函数
   EventCallback readCallback_;
--- src/Channel.cpp
+++ src/Channel.cpp
@@ -1,33 +1,85 @@
 #include "Channel.h"
 #include "EventLoop.h"
 
+#include <assert.h>
+#include <memory>
+#include <sstream>
+
 #include <sys/epoll.h>
 
 const int Channel::kNoneEvent = 0;
 const int Channel::kReadEvent = EPOLLIN | EPOLLPRI;
 const int Channel::kWriteEvent = EPOLLOUT;
 
-Channel::Channel(EventLoop *loop, int fd) : loop_(loop), fd_(fd) {}
+Channel::Channel(EventLoop *loop, int fd)
+    : loop_(loop), fd_(fd), events_(0), revents_(0), index_(-1), tied_(false),
+      eventHandling_(false), addedToLoop_(false) {}
 
 Channel::~Channel() {
-  //析构时不要close(fd),
+  //析构时不要close(fd),fd由Socket类进行RAII管理
+  assert(!eventHandling_);
+  assert(!addedToLoop_);
+  if (loop_->isInLoopThread()) {
+    assert(!loop_->hasChannel(this));
+  }
+}
+
+void Channel::tie(const std::shared_ptr<void> &obj) {
+  tie_ = obj;
+  tied_ = true;
 }
 
 void Channel::handleEvent() {
-  if (revents_ & EPOLLHUP) {
-    closeCallback_();
-  }
-  if (revents_ & EPOLLERR) {
-    errorCallback_();
-  }
-  if (revents_ & kReadEvent) {
-    readCallback_();
-  }
-  if (revents_ & kWriteEvent) {
-    writeCallback_();
+  std::shared_ptr<void> guard;
+  if (tied_) {
+    guard = tie_.lock();
+    if (guard) {
+      handleEventWithGuard();
+    }
+  } else {
+    handleEventWithGuard();
   }
 }
 
-void Channel::remove() { loop_->removeChannel(this); }
+void Channel::remove() {
+  assert(isNoneEvent());
+  addedToLoop_ = false;
+  loop_->removeChannel(this);
+}
 
-void Channel::update() { loop_->updateChannel(this); }
+void Channel::update() {
+  addedToLoop_ = true;
+  loop_->updateChannel(this);
+}
+
+std::string Channel::reventsToString() const {
+  return eventsToString(fd_, revents_);
+}
+
+std::string Channel::eventsToString() const {
+  return eventsToString(fd_, events_);
+}
+
+std::string Channel::eventsToString(int fd, int ev) {
+  std::ostringstream oss;
+  oss << fd << ": ";
+
+  if (ev & EPOLLIN)
+    oss << "IN ";
+  if (ev & EPOLLPRI)
+    oss << "PRI ";
+  if (ev & EPOLLOUT)
+    oss << "OUT ";
+  if (ev & EPOLLHUP)
+    oss << "HUP ";
+  if (ev & EPOLLRDHUP)
+    oss << "RDHUP ";
+  if (ev & EPOLLERR)
+    oss << "ERR ";
+  if (ev & EPOLLET)
+    oss << "ET "; // 边缘触发 (Edge Triggered)
+  if (ev & EPOLLONESHOT)
+    oss << "ONESHOT "; // 单次触发
+
+  return oss.str();
+}
